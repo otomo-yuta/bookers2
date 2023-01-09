b2bcf57e9638c031343a8f3126f78f77ff43e34b
EC2 Default User <ec2-user@ip-172-31-5-99.ap-northeast-1.compute.internal>
2023-01-09 12:10:47 +0000

    [Finish] good function

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
+    resource :favorites
     resources :book_comments, only: [:create, :destroy]
   end
   root to: 'homes#top'
