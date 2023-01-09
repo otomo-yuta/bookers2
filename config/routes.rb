34c3782e5ed8eed6bc724a73d2dc06bb464312f9
EC2 Default User <ec2-user@ip-172-31-5-99.ap-northeast-1.compute.internal>
2023-01-09 14:19:47 +0000

    [Finish]bookers2 addition

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
-    resource :favorites
+    resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
   root to: 'homes#top'
